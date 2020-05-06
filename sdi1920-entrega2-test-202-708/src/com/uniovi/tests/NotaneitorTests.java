9f506b12e288e4cd5ff0daddc46a8031bb38fe01
jaimeknd77 <jaimeknd77@gmail.com>
2020-05-06 21:09:06 +0200

    W1 Público: Registrarse como usuario
    Prueba1, Prueba2, Prueba3, Prueba4

--- sdi1920-entrega2-test-202-708/src/com/uniovi/tests/NotaneitorTests.java
+++ sdi1920-entrega2-test-202-708/src/com/uniovi/tests/NotaneitorTests.java
@@ -1,4 +1,5 @@
 package com.uniovi.tests;
+
 //Paquetes Java
 import java.util.List;
 //Paquetes JUnit 
@@ -8,27 +9,25 @@ import static org.junit.Assert.assertTrue;
 //Paquetes Selenium 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.*;
+
+import com.uniovi.tests.util.Internationalization;
 //Paquetes Utilidades de Testing Propias
 import com.uniovi.tests.util.SeleniumUtils;
 //Paquetes con los Page Object
 import com.uniovi.tests.pageobjects.*;
 
-
 //Ordenamos las pruebas por el nombre del m茅todo
-@FixMethodOrder(MethodSorters.NAME_ASCENDING) 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NotaneitorTests {
-	//En Windows (Debe ser la versi贸n 65.0.1 y desactivar las actualizacioens autom谩ticas)):
-	//static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-	//static String Geckdriver024 = "C:\\Path\\geckodriver024win64.exe";
-	//En MACOSX (Debe ser la versi贸n 65.0.1 y desactivar las actualizacioens autom谩ticas):
-	static String PathFirefox65 = "/Applications/Firefox 2.app/Contents/MacOS/firefox-bin";
-	//static String PathFirefox64 = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
-	static String Geckdriver024 = "/Users/delacal/Documents/SDI1718/firefox/geckodriver024mac";
-	//static String Geckdriver022 = "/Users/delacal/Documents/SDI1718/firefox/geckodriver023mac";
-	//Com煤n a Windows y a MACOSX
-	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024); 
-	static String URL = "https://localhost:8081";
+	
+	// En Windows (Debe ser la versi髇 65.0.1 y desactivar las actualizacioens
+	// autom醫icas)):
+	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver024 = "D:\\Jaime\\Repositorios\\sdi1920-entrega2-202-708\\sdi1920-entrega2-test-202-708\\geckodriver024win64.exe";
 
+	// Com鷑 a Windows y a MACOSX
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+	static String URL = "http://localhost:8081";
 
 	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
 		System.setProperty("webdriver.firefox.bin", PathFirefox);
@@ -37,206 +36,88 @@ public class NotaneitorTests {
 		return driver;
 	}
 
-
 	@Before
-	public void setUp(){
+	public void setUp() {
 		driver.navigate().to(URL);
 	}
+
 	@After
-	public void tearDown(){
+	public void tearDown() {
 		driver.manage().deleteAllCookies();
 	}
-	@BeforeClass 
+
+	@BeforeClass
 	static public void begin() {
-		//COnfiguramos las pruebas.
-		//Fijamos el timeout en cada opci贸n de carga de una vista. 2 segundos.
+		// COnfiguramos las pruebas.
+		// Fijamos el timeout en cada opci贸n de carga de una vista. 2 segundos.
 		PO_View.setTimeout(3);
 
 	}
+
 	@AfterClass
 	static public void end() {
-		//Cerramos el navegador al finalizar las pruebas
+		// Cerramos el navegador al finalizar las pruebas
 		driver.quit();
 	}
 
-	//PR01. Sin hacer /
+	/*
+	 * [Prueba1] Registro de Usuario con datos v醠idos.
+	 */
 	@Test
-	public void PR01() {
-		assertTrue("PR01 sin hacer", false);			
-	}
-
-	//PR02. Sin hacer /
-	@Test
-	public void PR02() {
-		assertTrue("PR02 sin hacer", false);			
-	}
-
-	//PR03. Sin hacer /
-	@Test
-	public void PR03() {
-		assertTrue("PR03 sin hacer", false);			
-	}
-	
-	//PR04. Sin hacer /
-	@Test
-	public void PR04() {
-		assertTrue("PR04 sin hacer", false);			
-	}
-	
-	//PR05. Sin hacer /
-	@Test
-	public void PR05() {
-		assertTrue("PR05 sin hacer", false);			
-	}
-	
-	//PR06. Sin hacer /
-	@Test
-	public void PR06() {
-		assertTrue("PR06 sin hacer", false);			
-	}
-	
-	//PR07. Sin hacer /
-	@Test
-	public void PR07() {
-		assertTrue("PR07 sin hacer", false);			
-	}	
-	
-	//PR08. Sin hacer /
-	@Test
-	public void PR08() {
-		assertTrue("PR08 sin hacer", false);			
-	}	
-	
-	//PR09. Sin hacer /
-	@Test
-	public void PR09() {
-		assertTrue("PR09 sin hacer", false);			
-	}	
-	//PR10. Sin hacer /
-	@Test
-	public void PR10() {
-		assertTrue("PR10 sin hacer", false);			
-	}	
-	
-	//PR11. Sin hacer /
-	@Test
-	public void PR11() {
-		assertTrue("PR11 sin hacer", false);			
-	}	
-	
-	//PR12. Sin hacer /
-	@Test
-	public void PR12() {
-		assertTrue("PR12 sin hacer", false);			
-	}	
-	
-	//PR13. Sin hacer /
-	@Test
-	public void PR13() {
-		assertTrue("PR13 sin hacer", false);			
-	}	
-	
-	//PR14. Sin hacer /
-	@Test
-	public void PR14() {
-		assertTrue("PR14 sin hacer", false);			
-	}	
-	
-	//PR15. Sin hacer /
-	@Test
-	public void PR15() {
-		assertTrue("PR15 sin hacer", false);			
-	}	
-	
-	//PR16. Sin hacer /
-	@Test
-	public void PR16() {
-		assertTrue("PR16 sin hacer", false);			
-	}	
-	
-	//PR017. Sin hacer /
-	@Test
-	public void PR17() {
-		assertTrue("PR17 sin hacer", false);			
-	}	
-	
-	//PR18. Sin hacer /
-	@Test
-	public void PR18() {
-		assertTrue("PR18 sin hacer", false);			
-	}	
-	
-	//PR19. Sin hacer /
-	@Test
-	public void PR19() {
-		assertTrue("PR19 sin hacer", false);			
-	}	
-	
-	//P20. Sin hacer /
-	@Test
-	public void PR20() {
-		assertTrue("PR20 sin hacer", false);			
-	}	
-	
-	//PR21. Sin hacer /
-	@Test
-	public void PR21() {
-		assertTrue("PR21 sin hacer", false);			
-	}	
-	
-	//PR22. Sin hacer /
-	@Test
-	public void PR22() {
-		assertTrue("PR22 sin hacer", false);			
-	}	
-	
-	//PR23. Sin hacer /
-	@Test
-	public void PR23() {
-		assertTrue("PR23 sin hacer", false);			
-	}	
-	
-	//PR24. Sin hacer /
-	@Test
-	public void PR24() {
-		assertTrue("PR24 sin hacer", false);			
-	}	
-	//PR25. Sin hacer /
-	@Test
-	public void PR25() {
-		assertTrue("PR25 sin hacer", false);			
-	}	
-	
-	//PR26. Sin hacer /
-	@Test
-	public void PR26() {
-		assertTrue("PR26 sin hacer", false);			
-	}	
-	
-	//PR27. Sin hacer /
-	@Test
-	public void PR27() {
-		assertTrue("PR27 sin hacer", false);			
-	}	
-	
-	//PR029. Sin hacer /
-	@Test
-	public void PR29() {
-		assertTrue("PR29 sin hacer", false);			
-	}
-
-	//PR030. Sin hacer /
-	@Test
-	public void PR30() {
-		assertTrue("PR30 sin hacer", false);			
-	}
-	
-	//PR031. Sin hacer /
-	@Test
-	public void PR31() {
-		assertTrue("PR31 sin hacer", false);			
-	}
-	
+	public void test01() {
+		SeleniumUtils.clickOption(driver, "signup", "class", "btn btn-primary");
 		
-}
+		SeleniumUtils.fillFormRegister(driver, "prueba1@prueba.com", "Prueba1", "Prueba1", "123456", "123456");
+		
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Nuevo usuario registrado", 2);
+		
+		SeleniumUtils.logout(driver);
+	}
 
+	/*
+	 * [Prueba2] Registro de Usuario con datos inv醠idos (email vac韔, nombre vac韔,
+	 * apellidos vac韔s).
+	 */
+	@Test
+	public void test02() {
+		SeleniumUtils.clickOption(driver, "signup", "class", "btn btn-primary");
+
+		SeleniumUtils.fillFormRegister(driver, "", "Prueba2", "Prueba2", "123456", "123456");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Reg韘trate como usuario", 2);
+
+		SeleniumUtils.fillFormRegister(driver, "prueba2@prueba.com", "", "Prueba2", "123456", "123456");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Reg韘trate como usuario", 2);
+
+		SeleniumUtils.fillFormRegister(driver, "prueba2@prueba.com", "Prueba2", "", "123456", "123456");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Reg韘trate como usuario", 2);
+	}
+
+	/*
+	 * [Prueba3] Registro de Usuario con datos inv醠idos (repetici髇 de contrase馻
+	 * inv醠ida).
+	 */
+	@Test
+	public void test03() {
+		SeleniumUtils.clickOption(driver, "signup", "class", "btn btn-primary");
+
+		SeleniumUtils.fillFormRegister(driver, "prueba3@prueba.com", "Prueba3", "Prueba3", "123456", "123457");
+
+		SeleniumUtils.checkKey(driver, "Error.signup.passwordConfirm.coincidence", Internationalization.getSPANISH());
+	}
+
+	/*
+	 * [Prueba4] Registro de Usuario con datos inv醠idos (email existente).
+	 */
+	@Test
+	public void test04() {
+		SeleniumUtils.clickOption(driver, "signup", "class", "btn btn-primary");
+
+		SeleniumUtils.fillFormRegister(driver, "prueba1@prueba.com", "Prueba1", "Prueba1", "123456", "123456");
+
+		SeleniumUtils.checkKey(driver, "Error.signup.email.duplicate", Internationalization.getSPANISH());
+	}
+
+}
